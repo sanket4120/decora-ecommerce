15a7fc706736b491d2a53d816420f6213d977c27
Sanket Jadhav <sanketvj.a01@gmail.com>
2022-04-24 09:29:50 +0530

    Added cart management

--- src/components/cards/productCard/ProductCard.js
+++ src/components/cards/productCard/ProductCard.js
@@ -1,37 +1,54 @@
-import './product-card.css';
+import { Link } from 'react-router-dom';
 import {
   addProductToWishlist,
   removeProductFromWishlist,
+  addProductToCart,
 } from '../../../actions/userActions';
 import { useUser } from '../../../context/userContext';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useMessage } from '../../../context/messageContext';
+import './product-card.css';
 
 function ProductCard({ product, showDetails }) {
   const {
     authState: { isAuthenticated },
-    wishlistState: { wishlist, loading },
+    wishlistState: { wishlist, loading: wishlistLoading },
+    cartState: { cart, loading: cartLoading },
     setWishlist,
+    setCart,
   } = useUser();
   const { setMessages } = useMessage();
   const navigate = useNavigate();
   const location = useLocation();
 
   let isInWishlist = false;
-  if (wishlist) {
-    if (wishlist.some((wishlistProduct) => wishlistProduct._id === product._id))
-      isInWishlist = true;
+  if (wishlist?.some((wishlistProduct) => wishlistProduct._id === product._id))
+    isInWishlist = true;
+
+  let isInCart = false;
+  if (cart?.some((cartProduct) => cartProduct._id === product._id)) {
+    isInCart = true;
   }
 
-  const handleClick = () => {
+  const toggleWishlist = () => {
     if (isAuthenticated) {
       isInWishlist
-        ? removeProductFromWishlist(setWishlist, setMessages, product._id)
+        ? removeProductFromWishlist(setWishlist, setMessages, product)
         : addProductToWishlist(setWishlist, setMessages, product);
     } else {
       navigate('/login', { state: { from: location.pathname } });
     }
   };
+
+  const addToCart = () => {
+    if (isAuthenticated) {
+      !isInCart && addProductToCart(setCart, setMessages, product);
+    } else {
+      navigate('/login', { state: { from: location.pathname } });
+    }
+  };
+
+  const isInStock = product.stock > 0;
 
   return (
     <div className='card flex-grow-1'>
@@ -46,8 +63,8 @@ function ProductCard({ product, showDetails }) {
           className={`btn btn-white icon-rounded card-badge-top-end ${
             isInWishlist && 'bg-primary'
           }`}
-          disabled={loading}
-          onClick={handleClick}
+          disabled={wishlistLoading}
+          onClick={toggleWishlist}
         >
           <i className='fa-regular fa-heart'></i>
         </button>
@@ -85,7 +102,21 @@ function ProductCard({ product, showDetails }) {
 
       {showDetails && (
         <div className='card-footer p-0'>
-          <button className='btn btn-primary w-100'>Add To Cart</button>
+          {isInCart ? (
+            <Link to='/cart' className='w-100'>
+              <button className='btn btn-primary w-100'>Go To Cart</button>
+            </Link>
+          ) : (
+            <button
+              className={`btn w-100 ${
+                isInStock ? 'btn-primary' : 'btn-secondary'
+              }`}
+              disabled={cartLoading || !isInStock}
+              onClick={addToCart}
+            >
+              {isInStock ? 'Add To Cart' : 'Out Of Stock'}
+            </button>
+          )}
         </div>
       )}
     </div>
--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,5 +1,87 @@
+import { Link } from 'react-router-dom';
+import HorizontalProductCard from '../../components/cards/horizontalProductCard/HorizontalProductCard';
+import { useUser } from '../../context/userContext';
+import './cart.css';
+
 const Cart = () => {
-  return <h1 className='txt-center'>Cart</h1>;
+  const {
+    cartState: { cart },
+  } = useUser();
+
+  const getCartTotal = () =>
+    cart?.reduce(
+      (acc, product) =>
+        (acc =
+          acc +
+          Number(product.qty) * Number(product.discountPrice ?? product.price)),
+      0
+    );
+
+  return (
+    <section className='my-6 minheight'>
+      <h1 className='txt-center mb-5 size-2'>My Cart</h1>
+      {cart?.length > 0 ? (
+        <section className='grid gap-2 minheight'>
+          <main className='col-12 col-md-7 col-lg-8'>
+            <div className='grid gap-2'>
+              {cart?.map((product) => (
+                <div className='col-12' key={product._id}>
+                  <HorizontalProductCard product={product} />
+                </div>
+              ))}
+            </div>
+          </main>
+
+          <aside className='col-12 col-md-5 col-lg-4 checkout'>
+            <div className='mb-5'>
+              <h4>Apply Coupan</h4>
+              <form className='my-3 flex align-items-center'>
+                <input
+                  type='text'
+                  className='input'
+                  placeholder='Enter your coupan code'
+                />
+                <button className='btn btn-secondary ml-1'>APPLY</button>
+              </form>
+            </div>
+
+            <ul className='list-unstyled border'>
+              {cart?.map((product) => (
+                <li
+                  className='list-item flex justify-content-between'
+                  key={product._id}
+                >
+                  <span>
+                    {product.title} &times; {product.qty}
+                  </span>
+                  <span className='ml-2'>
+                    {product.qty * (product.discountPrice ?? product.price)}
+                  </span>
+                </li>
+              ))}
+              <li className='list-item flex justify-content-between'>
+                <span className='fw-medium'>Total</span>
+                <span className='txt-primary'>{getCartTotal()}</span>
+              </li>
+              <li className='list-item'>
+                <button className='btn btn-primary w-100'>
+                  Proceed To Checkout
+                </button>
+              </li>
+            </ul>
+          </aside>
+        </section>
+      ) : (
+        <section className='txt-center mt-6'>
+          <h1 className='mb-3'>Your cart is empty!</h1>
+          <p className='mb-3'>Add items to it now</p>
+          <Link to='/shop'>
+            <button className='btn btn-primary'> Shop now </button>
+          </Link>
+        </section>
+      )}
+    </section>
+  );
 };
 
 export default Cart;
